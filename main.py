2dca65c804c6d1170e7e8064cefac24050023515
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2025-02-10 16:32:38 -0300

    Generator finished

--- main.py
+++ main.py
@@ -1,20 +1,70 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 import os
+import qrcode
+
+
 
 app = Flask(__name__)
 
-@app.route("/")
+IMG_FOLDER = os.path.join(app.root_path, 'static/images')
+
+qr = qrcode.QRCode(
+    version = 1,
+    box_size=10,
+    border=2.5
+)
+
+def generateQr(url):
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image()
+
+    img_path = os.path.join(IMG_FOLDER, 'qr_code.jpg')  # Nombre fijo, puedes cambiarlo
+    img.save(img_path)
+
+    return img_path
+
+
+@app.route("/", methods=['GET', 'POST'] )
 def home():
+    if request.method == 'POST':
+        #download_file( request.form.get('url') )
+        url = request.form.get('url')
+        if url:
+            generateQr(url)
+            return f'''
+                <img src="/static/images/qr_code.jpg" alt="QR Code">
+                <br>
+                <a href="/download">Descargar QR</a>
+            '''
+
     return render_template("index.html")
 
-@app.route('/download/<filename>')
-def download_file(filename):
+@app.route('/download')
+def download_file():
     return send_from_directory(
-        os.path.join(app.root_path, 'static', 'images'),
-        filename,
+        IMG_FOLDER,
+        'qr_code.jpg',
         as_attachment=True,  # Esto es lo que forzará la descarga
         mimetype='image/jpeg'  # Asegúrate de usar el tipo MIME adecuado para tu imagen
     )
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# from flask import Flask, render_template, request
+
+# app = Flask(__name__)
+
+# @app.route("/", methods=["GET", "POST"])
+# def index():
+#     if request.method == "POST":
+#         nombre = request.form.get("nombre")  # Captura el input
+#         return f"Hola, {nombre}!"  # Muestra el resultado en la misma página
+
+#     return render_template("index.html")  # Renderiza el HTML
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
---
+++ qrGenerator.py
@@ -0,0 +1,9 @@
+import qrcode
+import os
+
+#Ingresar URL de donde se quiere que le QR redireccione
+#url = input("Ingrese una URL: ")
+
+#Se crea el objeto QR
+
+
